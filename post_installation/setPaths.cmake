1c9cd55569336bfc7a9c2a2236133064c1cfdcab
sertschgi <58653179+sertschgi@users.noreply.github.com>
2023-07-23 22:22:38 +0200

    Update and rename setPaths.cmake to setPaths.sh

--- post_installation/setPaths.cmake
+++
@@ -1,8 +0,0 @@
-set(install_path @target_path_install@)
-
-file(APPEND "$ENV{HOME}/.bashrc" "export PATH=${install_path}:\$PATH\n")
-if (EXISTS "$ENV{HOME}/.bash_profile")
-    file(APPEND "$ENV{HOME}/.bash_profile" "export PATH=${install_path}:\$PATH\n")
-endif()
-
-message("added ${install_path}")

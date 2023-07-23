60cce91e86f9551e6a40ad2de23f8d13b3a54fbc
sertschgi <seppi@neureiter.bayern>
2023-07-23 22:19:18 +0200

    fixed some things

--- post_installation/setPaths.cmake
+++ post_installation/setPaths.cmake
@@ -1,7 +1,8 @@
-# InstallPath.cmake
-function(setPaths target_path_install)
-    file(APPEND "$ENV{HOME}/.bashrc" "export PATH=${target_path_install}:\$PATH\n")
-    if (EXISTS "$ENV{HOME}/.bash_profile")
-        file(APPEND "$ENV{HOME}/.bash_profile" "export PATH=${target_path_install}:\$PATH\n")
-    endif()
-endfunction()
+set(install_path @target_path_install@)
+
+file(APPEND "$ENV{HOME}/.bashrc" "export PATH=${install_path}:\$PATH\n")
+if (EXISTS "$ENV{HOME}/.bash_profile")
+    file(APPEND "$ENV{HOME}/.bash_profile" "export PATH=${install_path}:\$PATH\n")
+endif()
+
+message("added ${install_path}")

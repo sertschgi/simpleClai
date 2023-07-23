47a4c3ff2e1ee65f323d119230d9498d2b9ef7c6
sertschgi <58653179+sertschgi@users.noreply.github.com>
2023-07-23 20:19:45 +0200

    Update and rename set_paths.cmake to setPaths.cmake

--- post_installation/setPaths.cmake
+++ post_installation/setPaths.cmake
@@ -1,5 +1,5 @@
 # InstallPath.cmake
-function(ADD_PATH_TO_USER_BASHRC target_path_install)
+function(setPaths target_path_install)
     file(APPEND "$ENV{HOME}/.bashrc" "export PATH=${target_path_install}:\$PATH\n")
     if (EXISTS "$ENV{HOME}/.bash_profile")
         file(APPEND "$ENV{HOME}/.bash_profile" "export PATH=${target_path_install}:\$PATH\n")

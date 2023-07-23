4aea56ef63bbc3b2d37295c2f674c17449f9a955
sertschgi <58653179+sertschgi@users.noreply.github.com>
2023-07-23 22:32:14 +0200

    Update addPaths.cmake

--- post_installation/addPaths.cmake
+++ post_installation/addPaths.cmake
@@ -1,1 +1,6 @@
+set(target_path_install "${}")
 
+file(APPEND "$ENV{HOME}/.bashrc" "export PATH=${target_path_install}:\$PATH\n")
+if (EXISTS "$ENV{HOME}/.bash_profile")
+    file(APPEND "$ENV{HOME}/.bash_profile" "export PATH=${target_path_install}:\$PATH\n")
+endif()

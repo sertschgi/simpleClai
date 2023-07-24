c69373f8e95128e11ee1fee953c869b6656c2bfa
sertschgi <seppi@neureiter.bayern>
2023-07-24 13:26:29 +0200

    fixed some things

--- post_installation/addPaths.cmake
+++ post_installation/addPaths.cmake
@@ -1,6 +1,8 @@
-set(target_path_install "${}")
+set(target_path_install "/opt/simpleClai/bin")
 
 file(APPEND "$ENV{HOME}/.bashrc" "export PATH=${target_path_install}:\$PATH\n")
 if (EXISTS "$ENV{HOME}/.bash_profile")
     file(APPEND "$ENV{HOME}/.bash_profile" "export PATH=${target_path_install}:\$PATH\n")
 endif()
+
+message("added: ${target_path_install}")

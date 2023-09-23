e30dd8de616f443731cb49295716fe4e728af790
sert grc <seppi@neureiter.bayern>
2023-09-23 16:57:36 +0200

    added CMakeLists.txt

--- tests/dataset-test.sh
+++ tests/dataset-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sclai create dataset -n test-dataset -l dataset-test/labelmap.pbtxt -a dataset-test -i dataset-test/train
+sclai create dataset -n test-dataset -l dataset-test/labelmap.pbtxt -a dataset-test -m dataset-test/train
 sclai delete test-dataset
